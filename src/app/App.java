648f23572b1fd158848a023bf3d3cb48c94a467c
Lucas <lucas.silva8325@gmail.com>
2020-02-27 21:25:40 -0300

    Projeto criado

---
+++ src/app/App.java
@@ -0,0 +1,14 @@
+package app;
+
+import entity.UserTest;
+
+public class App {
+    public static void main(String[] args) {
+        
+        String name = "Andrew";
+
+        UserTest name1 = new UserTest(name, "Av.Pedro Friggi");
+
+        System.out.println(name1);
+    }
+}
---
+++ src/entity/UserTest.java
@@ -0,0 +1,63 @@
+package entity;
+
+public class UserTest {
+
+
+    private String nome;
+    private String end;
+
+    public UserTest() {
+    }
+
+    public UserTest(String nome, String end) {
+        this.nome = nome;
+        this.end = end;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "Inserted dates end=" + end + ", nome=" + nome;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserTest other = (UserTest) obj;
+        if (nome == null) {
+            if (other.nome != null)
+                return false;
+        } else if (!nome.equals(other.nome))
+            return false;
+        return true;
+    }
+    
+}
